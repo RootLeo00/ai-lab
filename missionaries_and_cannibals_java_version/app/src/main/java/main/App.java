c468d9f297dc3daa14ac08c4b42421331019128a
carnivuth <matti200042@gmail.com>
2023-04-21 18:24:36 +0200

    added depth first search

--- missionaries_and_cannibals_java_version/app/src/main/java/main/App.java
+++ missionaries_and_cannibals_java_version/app/src/main/java/main/App.java
@@ -5,10 +5,14 @@ package main;
 
 import aima.core.search.framework.SearchAgent;
 import aima.core.search.framework.problem.*;
+import aima.core.search.framework.qsearch.GraphSearch;
+import aima.core.search.framework.qsearch.TreeSearch;
 import aima.core.search.uninformed.BreadthFirstSearch;
+import aima.core.search.uninformed.DepthFirstSearch;
 import missionaries_cannibals.ActionFunctionMC;
 import missionaries_cannibals.GoalTestMC;
 import missionaries_cannibals.ResultFunctionMC;
+import missionaries_cannibals.StepCostFunctionMC;
 
 public class App {
  
@@ -16,14 +20,29 @@ public class App {
     public static void main(String[] args) {
         // start problem solution
         int[] initial_state= new int[3];
+        initial_state[0]=3;
+        initial_state[1]=3;
+        initial_state[2]=0;
 
-        Problem problem = new Problem(initial_state, new ActionFunctionMC(), new ResultFunctionMC(), new GoalTestMC(), null);
+        Problem problem = new Problem(initial_state, new ActionFunctionMC(), new ResultFunctionMC(), new GoalTestMC(), new StepCostFunctionMC());
         try {
-            SearchAgent agent =new SearchAgent(problem,new BreadthFirstSearch());
+            SearchAgent agent =new SearchAgent(problem,new BreadthFirstSearch(new GraphSearch()));
             // run search agent
-            //agent.execute(new AgentPercept(false, false, false, false, false));
+            //System.out.println(a.getName());
             System.out.println(agent.getActions());
             System.out.println(agent.isDone());
+            
+            SearchAgent agent1 =new SearchAgent(problem,new BreadthFirstSearch(new TreeSearch()));
+            // run search agent
+            //System.out.println(a.getName());
+            System.out.println(agent1.getActions());
+            System.out.println(agent1.isDone());
+
+            SearchAgent agent2 =new SearchAgent(problem,new DepthFirstSearch(new TreeSearch()));
+            // run search agent
+            //System.out.println(a.getName());
+            System.out.println(agent2.getActions());
+            System.out.println(agent2.isDone());
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
